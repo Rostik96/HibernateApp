22da4253480a50668959a7001f0c61f6e79fba43
Rostislav <zhistovskiy@vsk.ru>
2023-03-12 20:16:20 +0300

    Major refactor.

--- src/main/java/com/rost/App.java
+++ src/main/java/com/rost/App.java
@@ -1,13 +1,23 @@
 package com.rost;
 
+import org.hibernate.cfg.Configuration;
+import com.rost.model.Person;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        var configuration = new Configuration().addAnnotatedClass(Person.class);
+        var sessionFactory = configuration.buildSessionFactory();
+
+        try (sessionFactory) {
+            var session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            var person = session.get(Person.class, 1);
+            System.out.println(person);
+            session.getTransaction().commit();
+        }
     }
 }
---
+++ src/main/java/com/rost/model/Person.java
@@ -0,0 +1,29 @@
+package com.rost.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+}
